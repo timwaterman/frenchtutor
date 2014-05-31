374c70301d8cf667f9906e0c51cac1eccb94baa9
Tim Waterman <watermantium@gmail.com>
2014-05-31 00:08:28 -0400

    First test. Trying to get this integrated with Eclipse.

---
+++ src/frenchtutor/TutorTester.java
@@ -0,0 +1,11 @@
+package frenchtutor;
+
+public class TutorTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//I just want to ensure that this works
+	}
+
+}
